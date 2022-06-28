c50f04ff4292aa2fb8c1ed1e6c49cd79d9305c6c
Evgeniy Budaev <EBudaev@inno.tech>
2022-06-28 07:00:57 +0300

    JavaRush University

---
+++ src/javaRushUniversity/javaSyntax/level01/lesson07/Solution01.java
@@ -0,0 +1,9 @@
+package javaRushUniversity.javaSyntax.level01.lesson07;
+
+/*Напиши программу, которая выводит на экран надпись: "Амиго очень умный". */
+public class Solution01 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Амиго очень умный");
+    }
+}
