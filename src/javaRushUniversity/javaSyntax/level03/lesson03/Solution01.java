7fd807848bdf0099260e1f4f85c7ed4c16bd0418
Evgeniy Budaev <budaev.e@gmail.com>
2022-07-05 20:04:09 +0300

    Чтение чисел

---
+++ src/javaRushUniversity/javaSyntax/level03/lesson03/Solution01.java
@@ -0,0 +1,21 @@
+package javaRushUniversity.javaSyntax.level03.lesson03;
+
+import java.util.Scanner;
+
+/*
+Чтение чисел
+*/
+
+/* Считай с клавиатуры три целых числа.
+Выведи на экран их среднее арифметическое. */
+
+public class Solution01 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+        System.out.println((number1 + number2 + number3) / 3);
+    }
+}
